ed79bbb348fddb7276fff0ebdefdeeea6a330595
kekedezha <dezhac6@gmail.com>
2025-01-16 16:23:15 -0500

    added home screen image background

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
+
+import icedCoffeeImg from "@/assets/images/iced-coffee.png";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Coffee Shop</Text>
+      <ImageBackground
+        source={icedCoffeeImg}
+        resizeMode='cover'
+        style={styles.image}
+      >
+        <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
     </View>
   )
 }
@@ -20,6 +28,14 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 42,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
   }
 })
